a50b09cb46af0e4b95789420ad7ce726c2797beb
Sitio <vinicius.c.e@hotmail.com>
2019-08-22 09:40:35 -0300

    added graphic analisys

--- normalization.py
+++ normalization.py
@@ -1,11 +1,90 @@
 from sys import argv
 from collections import Counter
+import numpy as np
+from numpy import array as arr
+import matplotlib.pyplot as plt 
+import re
 
-datafile = open(argv[1])
-data = list(map(lambda x : x.split(",")[0:-1], datafile.readlines()))
-for x in data : x.pop(6)
+def date_split(date):
+    date = re.split("-|:| ", date)
+    year = int(date[0])
+    month = int(date[1])
+    day = int(date[2])
+    hour = int(date[3])
+    return hour, day, month, year 
 
 
-print(data[0])
-aux = [i[5] for i in data[1:]]
-print(len(Counter(aux).keys()))
+# Plots a relation between the average daily traffic per hour
+def avg_traffic_hour_daily(data):
+    # OBS: there are duplicated time stamps to separate more than one
+    # enviromental condition (p.e. if its foggy and cloudy there will be duplicates)
+    curr_day = 0
+    traffic_hour = []
+    for x in data[1:] :
+        hour, day, _, _ = date_split(x[6])
+        if (day != curr_day):
+            curr_day = day
+            traffic_hour.append(arr([0]*24))
+        traffic_hour[-1][hour] = x[-1]
+
+    avg = sum(traffic_hour)/len(traffic_hour)
+    plt.plot(range(0,24), avg)
+    plt.title("Análise do tráfego por hora")
+    plt.xlabel("Horas do dia (00h-24h)")
+    plt.ylabel("Média diária de tráfego")
+    plt.show()
+
+    return
+
+# Plot a realtion between weather descriptions and the average daily traffic per hour
+def avg_traffic_per_weather(data):
+
+    desc_main = {y:[0,0] for y in list(set([x[5] for x in data[1:]]))}
+    desc = {y:[0,0] for y in list(set([x[6] for x in data[1:]]))}
+    
+    for x in data[1:]:
+        desc_main[x[5]][0] += 1
+        desc_main[x[5]][1] += int(x[-1])
+        desc[x[6]][0] += 1
+        desc[x[6]][1] += int(x[-1])
+
+    counts = [x[0] if x[0]>0 else 1 for x in desc_main.values()]
+    sums = [x[1] for x in desc_main.values()]
+    desc_main_avg = arr(sums)/arr(counts)
+
+    counts = [x[0] if x[0]>0 else 1  for x in desc.values()]
+    sums = [x[1] for x in desc.values()]
+    desc_avg = arr(sums)/arr(counts)
+    
+    a = plt.figure(1)
+    plt.plot(list(desc.keys()), desc_avg)
+    plt.xticks(list(desc.keys()), rotation='vertical')
+    plt.margins(0)
+    plt.subplots_adjust(bottom=0.5)
+    plt.title("Análise do tráfego por descrição específica do clima")
+    plt.xlabel("Descrições específicas do clima")
+    plt.ylabel("Média diária de tráfego")
+    
+    b = plt.figure(2)
+    plt.plot(list(desc_main.keys()), desc_main_avg)
+    plt.title("Análise do tráfego por descrição geral do clima")
+    plt.xlabel("Descrições gerais do clima")
+    plt.ylabel("Média diária de tráfego")
+  
+    plt.show()
+    input()
+    
+    return
+
+
+def read_and_normalize():
+    datafile = open(argv[1])
+    data = list(map(lambda x : x.split(","), datafile.readlines()))
+
+    for i in range(1,len(data)):
+        data[i].pop(6) # Remove especific forecast data
+        data[i][-1] = int(data[i][-1][:-1]) # Cast traffic to int
+
+####################
+read_and_normalize()
+####################
