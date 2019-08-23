35bffd8ff1d5b8b242b570c7aebe2eb2ab31d037
VFerrari <vferrari@mpc.com.br>
2019-08-23 10:42:36 -0300

    Comment change

--- normal_eq.py
+++ normal_eq.py
@@ -6,7 +6,7 @@ def normal_equation(data):
 	# If the data is not a numpy array. 
 	arr = np.array(data)
 	
-	# Y: Values. X: Features
+	# Y: Target. X: Features/Examples
 	Y = arr[:,-1]
 	X = arr[:, :-1]
 	X = np.insert(X, 0, 1, axis=1)
