243a65362974178e46f57e77bb6bd8d62c3e05cf
Sitio <vinicius.c.e@hotmail.com>
2019-08-30 16:57:10 -0300

    Improved shuffling and changed iterations to epochs count (epoch controler functions were added as well).

--- gradient_descent.py
+++ gradient_descent.py
@@ -1,11 +1,11 @@
 import numpy as np
-from random import randint, sample
+from random import randint, sample, shuffle
 from sklearn import linear_model
 import time
 
-STEP_LIMIT = 10**(-3) # Step size accepted as convergence
-TIME_LIMIT = 180    # Descent limit given in seconds
-ITER_LIMIT = 10**6    # Maximum amount of iterations for the gradient
+# STEP_LIMIT = 10**(-3) # Step size accepted as convergence
+TIME_LIMIT = 5   # Descent limit given in seconds
+EPOCHS_LIMIT = 10**4    # Maximum amount of iterations for the gradient
 MINI_SIZE = 2         # Defines the size for the mini batch
 ALPHA = 0.1          # Learning rate
 
@@ -18,8 +18,7 @@ def numpy_and_bias(X, Y, T_set=(-10,10)):
 	X = np.array(X)
 	Y = np.array(Y)
 	X = np.insert(X, 0, 1, axis=1)
-	#T = np.array([[randint(-T_set[0],T_set[1])] for x in range(X.shape[1])]
-	T = None
+	T = np.array([[randint(-T_set[0],T_set[1])] for x in range(X.shape[1])])
 	
 	return (X,T,Y)
 
@@ -28,14 +27,6 @@ def cost(X, T, Y):
     m = Y.shape[0]
     cost = (1/(2*m))*sum((X.dot(T) - Y)**2)
     return cost[0] 
-
-def shuffle_samples(X, Y):
-    '''Return samples in randomized order'''
-    X = np.concatenate((X,Y),axis=1)
-    np.random.shuffle(X)
-    Y = X[:,[-1]]
-    X = X[:,:-1]
-    return X, Y 
 
 def descent(X, T, Y, type='b'):
     '''
@@ -52,14 +43,10 @@ def descent(X, T, Y, type='b'):
             T (Float 2dArray): The minimized cost coeficients obtained for the model.
     '''
 
-    # Setting time limit variables
-    end_time = 0
-    start_time = time.time()
-
     # Starting descent
-    count = 0
-    old_cost = cost(X, T, Y)
-    while (end_time - start_time) <= TIME_LIMIT:
+    boot_epoch_data(X, T, Y)
+    while (time.time() - start_time) <= TIME_LIMIT:
+
         # Getting new Thetas
         if (type=='b'):
             delta = batch_gradient(X, T, Y)
@@ -68,20 +55,13 @@ def descent(X, T, Y, type='b'):
         else:
             delta = stoch_gradient(X, T, Y)
         
-        T = T - ALPHA*delta
-        count += 1 # Count iteration
+        # Update gradients
+        T = T - ALPHA*delta 
 
-        # Updating hyperparameters
-        new_cost = cost(X, T, Y)
-        print(new_cost)
-        step = abs(old_cost - new_cost)
-        old_cost = new_cost
-        end_time = time.time()
-
-        # Testing gradient termination
-        if count >= ITER_LIMIT:
+        # Check termination
+        if epochs_count >= EPOCHS_LIMIT: 
             return T
-
+        
     print("NOTE: Time limit for descent exceded.")
     return T
 
@@ -89,33 +69,32 @@ def batch_gradient(X, T, Y):
     ''' Returns the gradiente calculated using all samples.'''
     m = Y.shape[0]
     gradient_vals = (1/m)*(X.dot(T) - Y).T.dot(X)
+   
+    # Update epoch global vars
+    update_epoch(X, T, Y, m, m)
+
     return (gradient_vals).T
 
 def minib_gradient(X, T, Y):
     ''' Returns the gradient calculate using a portion of the samples'''
     # Get index of first sample for the mini batch
-    base = minib_gradient.count
     m = Y.shape[0]
-    b = list(range(base, min((base + MINI_SIZE), m)))
+    b = samples_list[index : index+MINI_SIZE] # Get indexes for mini batch
     gradient_vals = (1/MINI_SIZE)*(X[b].dot(T) - Y[b]).T.dot(X[b])
 
-    # Update samples for mini batch
-    minib_gradient.count += MINI_SIZE
-    if minib_gradient.count >= m:
-        minib_gradient.count = 0 
+    # Update epoch global vars
+    update_epoch(X, T, Y, MINI_SIZE, m)
 
     return (gradient_vals).T
 
 def stoch_gradient(X, T, Y):
     ''' Returns the gradient calculated using a single random sample.'''
     m = Y.shape[0]
-    i = [stoch_gradient.count] # Get next sample
+    i = [samples_list[index]] # Get next sample
     gradient_vals = (X[i].dot(T) - Y[i])*(X[i]).T
 
-    # Update sample for stochastic
-    stoch_gradient.count += 1
-    if stoch_gradient.count >= m:
-        stoch_gradient.count = 0 
+    # Update epoch global vars
+    update_epoch(X, T, Y, 1, m)
 
     return gradient_vals
 
@@ -153,12 +132,43 @@ def normal_equation(X, Y):
 	theta = ((np.linalg.inv(square)).dot(X.T)).dot(Y)
 	return theta
 
-### Static variables ####
+### global epoch variables ####
 # References the current sample(s) used, ensuring that gradients
 # such as stoch and minib iterate through all the samples instead
-# of repeating samples in each iteration.
-minib_gradient.count = 0
-stoch_gradient.count = 0
+# of repeating samples in each iteration. Also provides a shuffled
+# list if samples indexes for randomizations and lists for keeping
+# epoch data.
+samples_list = []
+index = 0
+new_epoch = 0
+epochs_count = 0
+epochs_info = [[],[]] # Time and Cost per Epoch 
+start_time = 0
+
+def boot_epoch_data(X, T, Y):
+    global start_time, epochs_info, samples_list 
+    start_time = time.time() # Set starting time
+    epochs_info[0].append(cost(X, T, Y)) # Set starting cost
+    epochs_info[1].append(0.0)             # Set starting time
+    samples_list = list(range(Y.shape[0])) # Set and shuffle samples index
+    shuffle(samples_list)
+
+def update_epoch(X, T, Y, increment, bound):
+    '''Update epochs global variables when a epoch is completed'''
+    global epochs_info, epochs_count, index, samples_list, new_epoch 
+    
+    # Update samples count
+    index += increment
+    if index < bound : return
+    
+    # If epoch was completed
+    index = 0             # Reset samples index
+    epochs_info[0].append(cost(X, T, Y)) # Adds current epoch cost
+    epochs_info[1].append(time.time() - start_time) # Adds time until epoch is done
+    epochs_count += 1     # Count epoch
+    shuffle(samples_list) # Reshuffle samples
+    new_epoch = 0         # Lower new epoch flag
+
 #########################
 
 # Two ways of writing the gradient calculation
@@ -186,13 +196,10 @@ stoch_gradient.count = 0
 # T = np.array(T)
 
 # Add bias to features and generate the first set of T vals
-# X,T,Y = numpy_and_bias(X,Y)
+X,_,Y = numpy_and_bias(X,Y)
 
-# Shuffle samples to randomize minib and stoch gradients
-# X, Y = shuffle_samples(X,Y)
-
-
-
-# T = descent(X, T, Y)
-# print(T)
-# print("Cost =>",cost(X,T,Y))
+T = descent(X, T, Y, type='s')
+print(T)
+print("Cost =>",cost(X,T,Y))
+print(epochs_count)
+print(epochs_info[0][0],epochs_info[0][-1])
