eb002883d70f6b149c6e1c4bf29c571525b8319f
Sitio <vinicius.c.e@hotmail.com>
2019-08-31 09:35:26 -0300

    Added some testing

--- main.py
+++ main.py
@@ -1,14 +1,9 @@
 from importlib import import_module
+from random import randint
 import numpy as np
 model = import_module("first_model") 
 norm = import_module("normalization")
 desc = import_module("gradient_descent")
-
-# Run
-#T = desc.normal_equation(X,Y)
-# T = desc.descent(X, T, Y)
-# print(desc.predict(X,T))
-# print(Y)
 
 ### Getting Training Set ###
 # Returns the data without the header
@@ -25,15 +20,28 @@ X_val = norm.normalize_data(X_val, choice=1, features=feat_list)
 # DO NOT OVERWRITE THETAS
 X_val, _, Y_val = desc.numpy_and_bias(X_val, Y_val)
 
-# Train coeficients
-Y = np.ravel(Y)
-clf = desc.sk_regressor(X, Y)
-# Training results
-print("Training Score =>", clf.score(X,Y))
 
-# Validate
-Y_val = np.ravel(Y_val)
-print("Validation Score=>", clf.score(X_val,Y_val))
+# ### OUR DESCENT ###
+# T = desc.descent(X, T, Y)
+# print(desc.predict(X,T))
 
+# # Check some random predictions accuracy
+# for i in range(20):
+#     x = randint(0,Y_val.shape[0])
+#     print(int(desc.predict(X_val[[x]], T)), "=>", int(Y_val[[x]]))
 
-print(clf)
+# ### SKLEARN ###
+# # Train coeficients
+# Y = np.ravel(Y)
+# clf = desc.sk_regressor(X, Y)
+# # Training results
+# print("Training Score =>", clf.score(X,Y))
+
+# # Validate
+# Y_val = np.ravel(Y_val)
+# print("Validation Score=>", clf.score(X_val,Y_val))
+
+# # Check some random predictions accuracy
+# for i in range(20):
+#     x = randint(0,Y_val.shape[0])
+#     print(int(clf.predict(X_val[[x]])), "=>", int(Y_val[[x]]))
