e4e857b979563e7d2b3938425f150a446424e51b
Sitio <vinicius.c.e@hotmail.com>
2019-09-22 15:35:11 -0300

    Added neural network class, initialization and forward propagation (for a single sample)

---
+++ Projeto 2/neural.py
@@ -0,0 +1,38 @@
+import numpy as np
+from random import uniform, seed
+from datetime import datetime
+
+# Set seed based on current time
+seed(datetime.now())
+
+class Network:
+    def __init__(self, model, e=1):
+        '''Initializes coeficients tables with the network wheights
+        
+        Parameters: 
+            model (list) : List with the amount of nodes per layer (without bias)
+            e (float) : Range for the random coeficients initialization
+        '''
+        self.theta = []
+        # Use model array to define coeficients matrices dimensions
+        for (m,n) in zip(model[:-1], model[1:]):
+            # Initializes thetas with values between -e and e (and adds bias <m+1>)
+            table = [[uniform(-e,e) for j in range(m+1)] for i in range(n)]
+            self.theta.append(np.array(table)) # Add matrix to model
+
+    def forward(self, features):
+        '''Execute the forward propagation using the defined thetas
+        
+        Parameters: 
+            features (numpy.ndarray) : Column vector with input features (without bias)
+
+        Returns:
+            numpy.ndarray : Array with the propagated value for the output layer
+        '''
+        for table in self.theta : 
+            features = table.dot(np.vstack([[1], features]))
+        return features
+
+
+train = np.load("dataset/train.npz")
+X, Y = train['xs'], train['ys']
