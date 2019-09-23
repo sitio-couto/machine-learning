ffe93fa01c80570a8c974f3204234d4f1b5baf2f
VFerrari <vferrari@mpc.com.br>
2019-09-23 15:07:38 -0300

    Removing useless calculations from normalization for speedup

--- Project_2/normalization.py
+++ Project_2/normalization.py
@@ -11,24 +11,26 @@ def normalize_data(data, choice=1):
             data (array list):  transformed data (original data is lost).
     '''
 
-    # Data to be colected
-    means = np.apply_along_axis(np.mean, 0, data)
-    maxs = np.apply_along_axis(np.amax, 0, data)
-    mins = np.apply_along_axis(np.amin, 0, data)
-    stds = np.apply_along_axis(np.std, 0, data)
-    ranges = maxs - mins
-    
     #### Transforming the dataset ####
     # Min-max normalization
     if choice == 1:
+        mins = np.apply_along_axis(np.amin, 0, data)
+        maxs = np.apply_along_axis(np.amax, 0, data)
+        ranges = maxs - mins
         data = np.apply_along_axis(lambda x: (x - mins)/ranges, 1, data)
             
     # Standardization
     elif choice == 2:
+        means = np.apply_along_axis(np.mean, 0, data)
+        stds = np.apply_along_axis(np.std, 0, data)
         data = np.apply_along_axis(lambda x: (x - means)/stds, 1, data)
             
     # Mean normalization
     elif choice == 3:
+        means = np.apply_along_axis(np.mean, 0, data)
+        mins = np.apply_along_axis(np.amin, 0, data)
+        maxs = np.apply_along_axis(np.amax, 0, data)
+        ranges = maxs - mins
         data = np.apply_along_axis(lambda x: (x - means)/ranges, 1, data)
 
     return data
