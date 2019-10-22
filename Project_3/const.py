21c7d1c07b3b696c719a55902aef39f5e8d0a058
VFerrari <vferrari@mpc.com.br>
2019-10-21 22:00:29 -0300

    Starting project 3: Adding dataset and constants

---
+++ Project_3/const.py
@@ -0,0 +1,19 @@
+#Dimensions
+IMG_WIDTH = 28
+IMG_HEIGHT = 28
+IMG_SHAPE = (IMG_HEIGHT, IMG_WIDTH)
+
+#Classes
+N_CLASSES = 10
+CLASS_NAMES = {
+    0: 't-shirt/top',
+    1: 'trouser',
+    2: 'pullover',
+    3: 'dress',
+    4: 'coat',
+    5: 'sandal',
+    6: 'shirt',
+    7: 'sneaker',
+    8: 'bag',
+    9: 'ankle boot',
+}
